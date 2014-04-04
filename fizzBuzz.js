20f2cb0d186429ea3df16384ecc1d69dfa78eeb8
Andrew Benavides <zordonlives@gmail.com>
2014-04-03 17:36:03 -0700

    Initial Commit
    First One

---
+++ fizzBuzz.js
@@ -0,0 +1,11 @@
+
+for( i === 0; i >= 100; i += 1){
+        if( i % 15 === 0){
+              print("FizzBuzz");
+     } else if( i % 5 === 0){
+              print("Buzz");
+     } else if( i % 3 === 0){
+              print("Fizz");
+       } else { print(i); 
+    }
+}
