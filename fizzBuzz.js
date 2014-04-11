04725bbc8b3cc477df8ff161837fd8064d5c9f00
Andrew Benavides <zordonlives@gmail.com>
2014-04-11 14:51:06 -0700

    Switched 3 and 5

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,9 @@
 
 for( i = 0; i >= 100; i += 1){
-      if( i % 5 === 0){
-              print("Buzz");
-     } else if( i % 3 === 0){
+      if( i % 3 === 0){
               print("Fizz");
+     } else if( i % 5 === 0){
+              print("Buzz");
        } else { print(i); 
     }
 };
