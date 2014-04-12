14148fde4012a0c9aada8a4951879e09afaa3419
Andrew Benavides <zordonlives@gmail.com>
2014-04-12 13:45:44 -0700

    Comments Added
    also changed ++ to += for best practices

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,7 @@
 //FizzBuzz awesomeness brought to you by Andrew Benavides 
+// This code was inspired by the good folks at rosettacode.org
+// it is less code by using the ? and : operators and concatinating "fizz" and "buzz"
 
 var num = prompt("Hi there! Please be a gem and enter a number!")
 
-for (var i=1; i<=num; i++) console.log( (i % 3 === 0 ? 'Fizz' : '') + (i % 5 === 0 ? 'Buzz' : '') || i );
+for (var i=1; i<=num; i+=1) console.log( (i % 3 === 0 ? 'Fizz' : '') + (i % 5 === 0 ? 'Buzz' : '') || i );
